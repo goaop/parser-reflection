f7c8d2a654c846766c044e7c58b0231802b36d93
Alexander Lisachenko <lisachenko.it@gmail.com>
2015-11-05 14:03:55 +0300

    Add auto-detection of composer loader from spl stack

--- src/Locator/ComposerLocator.php
+++ src/Locator/ComposerLocator.php
@@ -5,6 +5,7 @@ namespace ParserReflection\Locator;
 
 use Composer\Autoload\ClassLoader;
 use ParserReflection\LocatorInterface;
+use ParserReflection\ReflectionException;
 
 class ComposerLocator implements LocatorInterface
 {
@@ -13,8 +14,19 @@ class ComposerLocator implements LocatorInterface
      */
     private $loader;
 
-    public function __construct(ClassLoader $loader)
+    public function __construct(ClassLoader $loader = null)
     {
+        if (!$loader) {
+            $loaders = spl_autoload_functions();
+            foreach ($loaders as $loader) {
+                if (is_array($loader) && $loader[0] instanceof ClassLoader) {
+                    $loader = $loader[0];
+                }
+            }
+            if (!$loader) {
+                throw new ReflectionException("Can not found a correct composer loader");
+            }
+        }
         $this->loader = $loader;
     }
 
