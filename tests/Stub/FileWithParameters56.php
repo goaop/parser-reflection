3ffb95ad82f697c8b4a073b9f46db5cdd26a7db1
Alexander Lisachenko <lisachenko.it@gmail.com>
2016-12-15 13:46:32 +0300

    Added tests for parameters in 5.6 and 7.0

---
+++ tests/Stub/FileWithParameters56.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Parser Reflection API
+ *
+ * @copyright Copyright 2015, Lisachenko Alexander <lisachenko.it@gmail.com>
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Go\ParserReflection\Stub;
+
+function arrayVariadicParameters(array ...$acceptsArrays) {}
+function callableVariadicParameters(callable ...$acceptsArrays) {}
+function constantExpressionAsDefault($value = 0.5 * 2 * 10, $another = __FUNCTION__ . 'test') {}
+function constantExponentiation($value = 0.5 * 2**2) {}
