bac0702c0af76bc6369d8fca878cec74ed890c67
Loren Osborn <linux_dr@yahoo.com>
2017-12-10 13:59:10 -0800

    Checking if PHP-Parser bug is in master branch.

--- tests/Stub/FileWithFunctions70.php
+++ tests/Stub/FileWithFunctions70.php
@@ -16,7 +16,28 @@ namespace Go\ParserReflection\Stub {
     {
     }
 
-    function builtInArgs(int $a, float $b, bool $c, string $d, object $e) : integer {}
+    // These types *LOOK LIKE* built in types, but they aren't.
+    function wrongBuiltInArgs(
+        integer $a,
+        double  $b,
+        boolean $c,
+        str     $d,
+        object  $e,
+        closure $f,
+        arr     $g
+    ) : integer {}
 
+    // These are the *REAL* type hints for the types above.
+    function builtInArgs(
+        int      $a,
+        float    $b,
+        bool     $c,
+        string   $d,
+                 $e, /* there is no generic object typehint */
+        callable $f,
+        array    $g
+    ) : int {}
+
+    // Remember, since integer isn't a valid builtin typehint, it's actually a class name.
     function mixedArgs(\DateTime $a, ReflectionMethod $b, bool $c, Fred\ReflectionEngine $d) : integer {}
 }
