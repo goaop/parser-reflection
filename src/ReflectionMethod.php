04e3727167dbb716ef600db633e273240a818609
lanfix <lanfix.object@gmail.com>
2021-05-28 11:43:56 +0300

    Compatibility fixes for reflection in php >= 8

--- src/ReflectionMethod.php
+++ src/ReflectionMethod.php
@@ -203,11 +203,11 @@ class ReflectionMethod extends BaseReflectionMethod
     /**
      * {@inheritDoc}
      */
-    public function invoke($object, $args = null)
+    public function invoke($object, ...$args)
     {
         $this->initializeInternalReflection();
 
-        return parent::invoke(...func_get_args());
+        return parent::invoke(...$args);
     }
 
     /**
