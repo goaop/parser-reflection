f8f20fb3f50be8434be502752d28eeaf7c467604
WalterWoshid <wotschel.valentin@googlemail.com>
2022-12-02 17:24:15 +0100

    Added TODO for ReflectionExtension

--- src/ReflectionExtension.php
+++ src/ReflectionExtension.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * Go! AOP framework
- *
- * @copyright Copyright 2022, Lisachenko Alexander <lisachenko.it@gmail.com>
- *
- * This source file is subject to the license that is bundled
- * with this source code in the file LICENSE.
- */
 declare(strict_types=1);
 /**
  * Parser Reflection API
@@ -16,13 +8,14 @@ declare(strict_types=1);
  * This source file is subject to the license that is bundled
  * with this source code in the file LICENSE.
  */
-
 namespace Go\ParserReflection;
 
 use ReflectionExtension as BaseExtension;
 
 /**
  * General parser-level reflection extension
+ *
+ * @todo Implement with lisachenko/z-engine
  */
 class ReflectionExtension extends BaseExtension
 {
