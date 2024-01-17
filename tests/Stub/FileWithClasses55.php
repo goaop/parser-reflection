3be7ab663904f1f04c2f5e87cd15e74f47b0a3a2
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-01-17 23:57:12 +0700

    add more test

--- tests/Stub/FileWithClasses55.php
+++ tests/Stub/FileWithClasses55.php
@@ -94,7 +94,7 @@ abstract class AbstractClassWithMethods extends BaseClass
         return 10;
     }
 
-    private function testParam($a, $b = null, $d = self::TEST, $e = M_PI, $f = \M_PI) {}
+    private function testParam($a, $b = null, $d = self::TEST, $e = M_PI, $f = \M_PI, $g = [], $h = false, $i = true) {}
 }
 
 class ClassWithProperties
