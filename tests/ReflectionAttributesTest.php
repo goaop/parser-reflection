773ca7b61740b7825a735c435c0647bdcc5a4f13
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-02-02 10:51:16 +0700

    test for class const

--- tests/ReflectionAttributesTest.php
+++ tests/ReflectionAttributesTest.php
@@ -21,9 +21,37 @@ class ReflectionAttributesTest extends TestCase
 
         foreach ($parameters as $parameter) {
             $attributes = $parameter->getAttributes();
+            $originalReflectionParameter = new \ReflectionParameter('Go\ParserReflection\Stub\authenticate', $parameter->getName());
+
             foreach ($attributes as $attribute) {
-                $originalReflectionParameter = new \ReflectionParameter('Go\ParserReflection\Stub\authenticate', $parameter->getName());
                 $originalAttribute = current($originalReflectionParameter->getAttributes($attribute->getName()));
+
+                $this->assertInstanceOf(ReflectionAttribute::class, $attribute);
+                $this->assertSame($originalAttribute->getName(), $attribute->getName());
+                $this->assertSame($originalAttribute->getName(), $attribute->getName());
+                $this->assertSame($originalAttribute->getTarget(), $attribute->getTarget());
+                $this->assertSame($originalAttribute->getTarget(), $attribute->getTarget());
+                $this->assertSame($originalAttribute->getArguments(), $attribute->getArguments());
+                $this->assertSame($originalAttribute->isRepeated(), $attribute->isRepeated());
+            }
+        }
+    }
+
+    public function testGetAttributeOnClassConst()
+    {
+        $this->setUpFile(__DIR__ . '/Stub/FileWithClassConst80.php');
+
+        $fileNamespace = $this->parsedRefFile->getFileNamespace('Go\ParserReflection\Stub');
+        $constants = $fileNamespace->getClass('Go\ParserReflection\Stub\FileWithClassConstAttribute')->getConstants();
+
+        foreach (array_keys($constants) as $constant) {
+            $reflectionClassConst = new ReflectionClassConstant('Go\ParserReflection\Stub\FileWithClassConstAttribute', $constant);
+            $attributes = $reflectionClassConst->getAttributes();
+            $originalReflectionClassConst = new \ReflectionClassConstant('Go\ParserReflection\Stub\FileWithClassConstAttribute', $constant);
+
+            foreach ($attributes as $attribute) {
+
+                $originalAttribute = current($originalReflectionClassConst->getAttributes($attribute->getName()));
 
                 $this->assertInstanceOf(ReflectionAttribute::class, $attribute);
                 $this->assertSame($originalAttribute->getName(), $attribute->getName());
--- tests/Stub/FileWithClassConst80.php
+++ tests/Stub/FileWithClassConst80.php
@@ -13,5 +13,6 @@ namespace Go\ParserReflection\Stub;
 
 class FileWithClassConstAttribute
 {
+    #[\SomeAttribute]
     public const FOO = 1;
 }
