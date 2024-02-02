235daf757ce595011987cd3bbed3007b7067fbd1
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-02-02 09:45:45 +0700

    get from parent attributes

--- src/Traits/AttributeResolverTrait.php
+++ src/Traits/AttributeResolverTrait.php
@@ -13,13 +13,6 @@ declare(strict_types=1);
 namespace Go\ParserReflection\Traits;
 
 use Go\ParserReflection\ReflectionAttribute;
-use Go\ParserReflection\ReflectionException;
-use PhpParser\Node\Param;
-use PhpParser\Node\Stmt\Class_;
-use PhpParser\Node\Stmt\ClassConst;
-use PhpParser\Node\Stmt\ClassMethod;
-use PhpParser\Node\Stmt\Function_;
-use PhpParser\Node\Stmt\Property;
 
 trait AttributeResolverTrait
 {
@@ -30,32 +23,12 @@ trait AttributeResolverTrait
     {
         $attributes = [];
 
-        /** @var Class_|ClassMethod|Property|ClassConst|Function_|Param $node  */
-        $node = $this->getNode();
-
-        foreach ($node->attrGroups as $attrGroup) {
-            foreach ($attrGroup->attrs as $attr) {
-                $attributeName = $name === null
-                    ? $attr->name->toString()
-                    : $name;
-
-                $attributes[] = new ReflectionAttribute($attributeName, $this, $flags, $attr->args, $this->resolveTarget($attributeName, $flags));
-            }
+        $originalAttributes = parent::getAttributes($name, $flags);
+        foreach ($originalAttributes as $originalAttribute) {
+            $attributeName = $originalAttribute->getName();
+            $attributes[] = new ReflectionAttribute($attributeName, $this, $flags, $originalAttribute->getArguments(), $originalAttribute->getTarget());
         }
 
         return $attributes;
-    }
-
-    private function resolveTarget(string $name, int $flags): int
-    {
-        $originalAttributes = parent::getAttributes($name, $flags);
-
-        foreach ($originalAttributes as $originalAttribute) {
-            if ($originalAttribute->getName() === $name) {
-                return $originalAttribute->getTarget();
-            }
-        }
-
-        throw new ReflectionException(sprintf('target not found on attribute %s', $name));
     }
 }
