209fe1e51433bed0a218b26205c47b5f418e278f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-02-02 09:02:31 +0700

    fill reflector

---
+++ tests/Stub/FileWithParameters80.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+/**
+ * Parser Reflection API
+ *
+ * @copyright Copyright 2015, Lisachenko Alexander <lisachenko.it@gmail.com>
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Go\ParserReflection\Stub;
+
+function authenticate(
+    string $username,
+    #[\SensitiveParameter]
+    string $password
+) {
+}
